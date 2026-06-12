95b96bad1aa6ac1c010de2f3570630e190777f3c
saksham mishra <102840790+sakshammishra5@users.noreply.github.com>
2026-06-12 10:27:48 +0530

    Add: anagram-groups - submission-15

---
+++ Data Structures & Algorithms/anagram-groups/submission-15.py
@@ -0,0 +1,18 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        anagramMap={}
+
+        for i in range(len(strs)):
+                sorted_s= "".join(sorted(strs[i]))
+                if sorted_s in anagramMap:
+                    anagramMap[sorted_s].append(strs[i])
+                else:
+                    anagramMap[sorted_s]=[strs[i]] 
+
+        output=[]
+        for value in anagramMap.values():
+            output.append(value)     
+
+
+        return output
